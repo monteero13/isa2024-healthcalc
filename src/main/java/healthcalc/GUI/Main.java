09abf82c40a95cf3edd8fd85ccee56f6b42d301a
sisisis <sisisi@ezamepl.com>
2024-04-17 23:08:13 +0200

    Version de Main final

--- src/main/java/healthcalc/GUI/Main.java
+++ src/main/java/healthcalc/GUI/Main.java
@@ -1,9 +1,13 @@
 package healthcalc.GUI;
 
+import healthcalc.HealthCalcImpl;
+
 public class Main {
     public static void main(String[] args) {
         Vista vista = new Vista();
         HealthCalcImpl model = new HealthCalcImpl();
         Controlador controlador = new Controlador(vista, model);
+        vista.setControlador(controlador); // Añade esta línea
+        vista.mostrar(); // Añade esta línea
     }
 }
