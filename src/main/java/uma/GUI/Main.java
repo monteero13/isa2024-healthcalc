03a74b5b6094096fa4a59b94b7422ef0d5cf3074
monteero13 <13albertomontero@gmail.com>
2024-05-14 10:02:41 +0200

    Modificar Main para probar nuevo patrón Singular

--- src/main/java/uma/GUI/Main.java
+++ src/main/java/uma/GUI/Main.java
@@ -5,7 +5,7 @@ import uma.healthcalc.HealthCalcImpl;
 public class Main {
     public static void main(String[] args) {
         Vista vista = new Vista();
-        HealthCalcImpl model = new HealthCalcImpl();
+        HealthCalcImpl model = HealthCalcImpl.getInstance();
         Controlador controlador = new Controlador(vista, model);
         vista.setControlador(controlador); // Añade esta línea
         vista.mostrar(); // Añade esta línea
