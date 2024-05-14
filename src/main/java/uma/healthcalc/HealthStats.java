1ff0291b1d6cba68548e861946e8097285e9447b
monteero13 <13albertomontero@gmail.com>
2024-05-14 11:08:18 +0200

    Añadir interfaz apartado b

---
+++ src/main/java/uma/healthcalc/HealthStats.java
@@ -0,0 +1,12 @@
+package uma.healthcalc;
+
+public interface HealthStats {
+
+    public float alturaMedia();
+    public float pesoMedio();
+    public float edadMedia();
+    public float bmrMedio();
+    public int numSexoH();
+    public int numSexoM();
+    public int numTotalPacientes();
+}
