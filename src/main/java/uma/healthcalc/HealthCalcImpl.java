556d96d939671496221b721958609b0612c3355e
sisisis <sisisi@ezamepl.com>
2024-06-04 22:15:15 +0200

    Clase de la implementación de la calculadora modificada para que implemente las dos nuevas interfaces, que utilizan la clase PersonImpl que a su vez implementa la nueva interfaz Person, la cual utiliza la nueva clase enum Gender a su vez

--- src/main/java/uma/healthcalc/HealthCalcImpl.java
+++ src/main/java/uma/healthcalc/HealthCalcImpl.java
@@ -1,6 +1,6 @@
 package uma.healthcalc;
 
-public class HealthCalcImpl implements HealthCalc {
+public class HealthCalcImpl implements CardiovascularMetrics, MetabolicMetrics {
 
 	private static HealthCalcImpl instance;
 	
@@ -13,51 +13,51 @@ public class HealthCalcImpl implements HealthCalc {
 		}
 		return instance;
 	}
-	
-	@Override
-	public float idealWeight(int height, char string) throws Exception {
-		Character genero = Character.toUpperCase(string);
-		if (height > 272) {
-			throw new IllegalArgumentException(
-					"La altura supera el límite establecido puesto que el máximo recogido es de 1.72 metros");
-		} else if (height < 0) {
-			throw new IllegalArgumentException("La altura introducida es negativa");
-		} else if (height < 50) {
-			throw new IllegalArgumentException(
-					"La altura se encuentra por debajo del límite establecido puesto que el mínimo recogido es de 0.5 metros");
-		} else if (genero != 'H' && genero != 'M') {
-			throw new IllegalArgumentException("Solo se acepta entradas de género 'H' para hombres y 'M' para mujeres");
-		} else {
-			if (genero == 'H') {
-				return height - 100 - ((height - 150) / 4);
-			} else {
-				return height - 100 - ((height - 150) / 2.5f);
-			}
-		}
-	}
 
 	@Override
-	public float basalMetabolicRate(float weight, int height, char gender, int age) throws Exception {
-		Character genero = Character.toUpperCase(gender);
-		float BMR = 0;
-		if (15 > weight || weight > 500) {
+	public double basalMetabolicRate(Person person) {
+		Gender genero = person.gender();
+		double BMR = 0;
+		if (15 > person.weight() || person.weight() > 500) {
 			throw new IllegalArgumentException(
 					"Peso incorrecto: Solo se aceptan valores dentro del rango [15kg,500kg]");
-		} else if (50 > height || height > 272) {
+		} else if (50 > person.height() || person.height() > 272) {
 			throw new IllegalArgumentException(
 					"Altura incorrecta: Solo se aceptan valores dentro del rango [40cm,275cm]");
 		} else {
-			if (genero != 'H' && genero != 'M') {
+			if (genero != Gender.MALE && genero != Gender.FEMALE) {
 				throw new IllegalArgumentException(
 						"Género incorrecto: Solo se acepta entradas de género 'H' para hombres y 'M' para mujeres");
-			} else if (0 > age || age > 100) {
+			} else if (0 > person.age() || person.age() > 100) {
 				throw new IllegalArgumentException("Edad incorrecta: Solo se aceptan valores dentro del rango [0,100]");
-			} else if (genero == 'H') {
-				BMR = (10 * weight) + (6.25f * height) - (5 * age) + 5;
+			} else if (genero == Gender.MALE) {
+				BMR = (10 * person.weight()) + (6.25d * person.height()) - (5 * person.age()) + 5;
 			} else {
-				BMR = (10 * weight) + (6.25f * height) - (5 * age) - 161;
+				BMR = (10 * person.weight()) + (6.25d * person.height()) - (5 * person.age()) - 161;
 			}
 		}
 		return BMR;
+	}
+
+	@Override
+	public double getIdealBodyWeight(Person person) {
+		Gender genero = person.gender();
+		if (person.height() > 272) {
+			throw new IllegalArgumentException(
+					"La altura supera el límite establecido puesto que el máximo recogido es de 1.72 metros");
+		} else if (person.height() < 0) {
+			throw new IllegalArgumentException("La altura introducida es negativa");
+		} else if (person.height() < 50) {
+			throw new IllegalArgumentException(
+					"La altura se encuentra por debajo del límite establecido puesto que el mínimo recogido es de 0.5 metros");
+		} else if (genero != Gender.MALE && genero != Gender.FEMALE) {
+			throw new IllegalArgumentException("Solo se acepta entradas de género 'H' para hombres y 'M' para mujeres");
+		} else {
+			if (genero == Gender.MALE) {
+				return person.height() - 100 - ((person.height() - 150) / 4d);
+			} else {
+				return person.height() - 100 - ((person.height() - 150) / 2.5d);
+			}
+		}
 	}
 }
